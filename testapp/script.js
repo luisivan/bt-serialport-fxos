0a074d4530750b2916e7a2f726d50ef42f5f9628
Luis Iván Cuende <me@luisivan.net>
2014-08-07 13:17:27 +0200

    At least it tries to send data

--- testapp/script.js
+++ testapp/script.js
@@ -4,10 +4,24 @@ function test() {
 	console.log('hi2')
 
 	navigator.mozBluetooth.getDefaultAdapter().onsuccess = function(e) {
-		console.log(e)
 		adapter = e.target.result
-		if (typeof adapter.sendSerialPort == 'function')
-			alert('sendSerialPort is defined!')
+		adapter.getPairedDevices().onsuccess = function(e) {
+			var devices = e.target.result
+			for (var i in devices) {
+				if (devices[i].address == "a4:db:30:21:98:a3") {
+					adapter.connect(devices[i])
+					adapter.sendSerialPort("a4:db:30:21:98:a3", "ola k ase")
+				}
+			}
+			
+		}
+		
+
+		adapter.onserialportdata = function(e) {
+			console.log(e)
+			alert('onSerialPortData fired!')
+		}
+		adapter.sendSerialPort('address', 'oi')
 	}
 }
 
