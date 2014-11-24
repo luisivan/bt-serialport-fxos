7984c217ec49cf04b16b872ce69a7ce88e8d347f
Luis Iván Cuende <me@luisivan.net>
2014-11-24 11:36:16 +0100

    Improvements for the NodeJS test app

--- btsp/index.js
+++ btsp/index.js
@@ -1,29 +1,40 @@
-var serial = require('serialport')
+var readline = require('readline'),
+	serial = require('serialport')
 
-serial.list(function (err, ports) {
-
-    if (err || ports.length == 0)
-        return console.log ( (err) ? err : 'No serial ports available' )
-
-    ports.forEach(function(port) {
-        console.log(port.comName)
-        console.log(port.pnpId)
-        console.log(port.manufacturer)
-    })
+var rl = readline.createInterface({
+  	input: process.stdin,
+  	output: process.stdout
 })
 
-var serialPort = new serial.SerialPort('/dev/rfcomm0', {
-    baudrate: 57600
+rl.on('line', function (cmd) {
+  	if (cmd == 's') {
+  		console.log('Sending some data')
+  		sendData()
+  	} else if (cmd == 'c') {
+  		console.log('Bye')
+  		// Force it, otherwise it won't work
+  		process.kill(process.pid, 'SIGKILL')
+  	}
 })
 
-serialPort.on('open', function () {
+var serialPort
 
-    console.log('Serial Port opened')
-    serialPort.on('data', function(data) {
-        console.log('Data received: ' + data)
-    })
+(function() {
+	serialPort = new serial.SerialPort('/dev/rfcomm0', {
+		baudrate: 57600
+	})
+	serialPort.on('data', function(data) {
+	    console.log('Data received: ' + data)
+	})
+	console.log('Serial Port opened')
+})()
+
+function sendData() {
     serialPort.write('Plz receive this data\n', function(err, results) {
         console.log('Err is ' + err)
+        // results always = 22, which must be this Bluetooth event:
+        // #define L2CEVT_L2CA_CONNECT_RSP 22 /* Upper layer connect response */
+        // It's sending a configuration response I guess
         console.log('Results are ' + results)
     })
-})
+}
